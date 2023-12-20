349ffb6badc113c968efb2677383725713999f6f
mpilopillz <mpilo.dlamini@yahoo.com>
2023-12-20 13:35:36 +0200

    adding customer controller

---
+++ nest-dvd-rental/src/customer/customer.controller.ts
@@ -0,0 +1,40 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CustomerService } from './customer.service';
+
+@Controller('customer')
+export class CustomerController {
+  constructor(private readonly customerService: CustomerService) {}
+
+  @Post()
+  create(@Body() createCustomerDto) {
+    return this.customerService.create(createCustomerDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.customerService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.customerService.findOne(+id);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateCustomerDto) {
+    return this.customerService.update(+id, updateCustomerDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.customerService.remove(+id);
+  }
+}
