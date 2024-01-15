6a34ff431fa38098168f8b8a08a6f2417683ca9d
mpilopillz <mpilo.dlamini@yahoo.com>
2024-01-15 21:46:01 +0200

    suceffully reading data form DB

--- DVDRentalJDBC/src/main/java/org/example/Main.java
+++ DVDRentalJDBC/src/main/java/org/example/Main.java
@@ -1,14 +1,21 @@
 package org.example;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
 
-        try (Connection connection = DriverManager.getConnection(Application.url, Application.userName, Application.password)) {
-            System.out.println(connection);
+        try (
+                Connection connection = DriverManager.getConnection(Application.url, Application.userName, Application.password);
+                PreparedStatement statement = connection.prepareStatement("SELECT * from Customer");
+                ) {
+//            System.out.println(connection);
+
+            ResultSet resultSet = statement.executeQuery();
+
+            while(resultSet.next()) {
+                System.out.println(resultSet.getString("last_name"));
+            }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
