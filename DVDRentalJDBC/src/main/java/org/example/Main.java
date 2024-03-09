5f6a81857348987c853431ec47e56aaf58fbe5f9
mpilopillz <mpilo.dlamini@yahoo.com>
2024-03-09 13:16:06 +0200

    lenring excecute

--- DVDRentalJDBC/src/main/java/org/example/Main.java
+++ DVDRentalJDBC/src/main/java/org/example/Main.java
@@ -24,17 +24,21 @@ public class Main {
 
     public static void main(String[] args) {
         try(Connection connection = DriverManager.getConnection(Application.url, Application.userName, Application.password)) {
-            simpleDeleteWithExcecuteUpdate(connection);
+//            simpleDeleteWithExcecuteUpdate(connection);
 //                        simpleUpdateWithExcecuteUpdate(connection);
 //                        simpleReadWithExecuteQuery(connection);
 //            simpleInsertWithExcecuteUpdate(connection);
+            simpleReadWithExecute(connection);
         } catch (SQLException e) {
 
         }
     }
 
+
+
+    // DELETE
     private static void simpleDeleteWithExcecuteUpdate(Connection connection) throws SQLException {
-        String sql = "DELETE FROM country where country='Wakanda'";
+        String sql = "DELETE FROM country where country='Wakanda'"; // will delete al occurences
 
         PreparedStatement preparedStatement = connection.prepareStatement(sql);
 
@@ -46,6 +50,8 @@ public class Main {
             System.out.println("No update");
         }
     }
+
+    // UPDATE
     private static void simpleUpdateWithExcecuteUpdate(Connection connection) throws SQLException {
         String sql = "UPDATE \"country\" SET \"country\" = 'Eswatini / Swaziland' WHERE \"country\" = 'Eswatini'";
 
@@ -60,6 +66,7 @@ public class Main {
         }
     }
 
+    // CREATE
     private static void simpleInsertWithExcecuteUpdate(Connection connection) throws SQLException {
         String sql = "INSERT INTO country (country) VALUES ('Wakanda')";
 
@@ -73,6 +80,8 @@ public class Main {
             System.out.println("No update");
         }
     }
+
+    // READ
     private static void simpleReadWithExecuteQuery(Connection connection) throws SQLException {
         String sql = "select first_name, email from customer";
 
@@ -84,5 +93,24 @@ public class Main {
             System.out.println(".");
         }
         System.out.println();
+    }
+
+    // Excecute
+    // Returns a boolean for if there is a result set or not
+    private static void simpleReadWithExecute(Connection connection) throws SQLException {
+        String sql = "select actor_id, first_name, last_name from actor";
+
+        var statement = connection.prepareStatement(sql);
+
+        var result = statement.execute();
+
+        if (result) {
+            var resultSet = statement.getResultSet();
+            while(resultSet.next()) {
+                System.out.println("." + resultSet);
+            }
+            System.out.println();
+        }
+
     }
 }
