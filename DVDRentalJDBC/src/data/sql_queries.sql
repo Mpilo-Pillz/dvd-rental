e61418013a859f98fd16637c9c4ef390181f2a95
mpilopillz <mpilo.dlamini@yahoo.com>
2024-03-09 18:27:51 +0200

    saving sql queries used to testing

---
+++ DVDRentalJDBC/src/data/sql_queries.sql
@@ -0,0 +1,10 @@
+select * from country
+select * from film_actor
+select * from actor
+select * from city
+
+SELECT first_name, last_name FROM actor WHERE last_name LIKE '%ashingto%';
+
+SELECT first_name, last_name FROM actor WHERE last_name LIKE '%ag%';
+
+SELECT COUNT(*) AS count FROM staff;
